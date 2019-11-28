4fd81a4258f2ec5ecbbb45a65306d924b8a79c44
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-28 14:40:32 +0100

    Nettoyage du code

--- src/main/java/ex1/Calcul.java
+++ src/main/java/ex1/Calcul.java
@@ -4,7 +4,7 @@ package ex1;
  * Classe qui réalise des opérations arythmétiques
  * @author DIGINAMIC
  */
-public class calcul {
+public class Calcul {
 
 	/** Méthode qui retourne l'addition des paramètres a et b 
 	 * 
@@ -13,16 +13,16 @@ public class calcul {
 	 * @return int 
 	 */
 	public int addition(int a, int b){
-		return a+b;
+		return a + b;
 	}
 	
-	/**
+	/** Méthode qui retourne la soustraction des paramètres a et b 
 	 * 
 	 * @param a
 	 * @param b
-	 * @return
+	 * @return int
 	 */
-	public int get2(int a, int b){
-		return a-b;
+	public int soustraction(int a, int b){
+		return a - b;
 	}
 }
--- src/main/java/ex1/Entreprise.java
+++ src/main/java/ex1/Entreprise.java
@@ -2,17 +2,132 @@ package ex1;
 
 import java.util.Date;
 
-public class entreprise {
+public class Entreprise {
 
-	public int Siret;
-	public String Nom;
-	public String adresse;
-	public Date date_Creation;
+
+	/** Attributs de la classe Entreprise
+	 * @param siret en tant qu'entier
+	 * @param nom en tant que string
+	 * @param adresse en tant que string
+	 * @param dateCreation en tant que date
+	 * @param CAPITAL_MAX en tant que constante
+	 */
 	
-	public static final int capitalMax = 3000000;
+	private int siret;
+	private String nom;
+	private String adresse;
+	private Date dateCreation;
+
+	public static final int CAPITAL_MAX = 3000000;
 	
+
+	/** Constructor de la classe Entreprise
+	 * @param siret
+	 * @param nom
+	 * @param adresse
+	 * @param dateCreation
+	 */
+	public Entreprise(int siret, String nom, String adresse, Date dateCreation) {
+		super();
+		this.siret = siret;
+		this.nom = nom;
+		this.adresse = adresse;
+		this.dateCreation = dateCreation;
+	}
+
+
+
+	/* cette methode n'a pas de contenu... mis en commentaire temporairement
 	public void Afficher_statut(){
 		
+	}
+	 */
+
+
+
+
+	/** Getter
+	 * @return the siret
+	 */
+	public int getSiret() {
+		return siret;
+	}
+
+
+
+
+
+	/** Setter
+	 * @param siret the siret to set
+	 */
+	public void setSiret(int siret) {
+		this.siret = siret;
+	}
+
+
+
+
+
+	/** Getter
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+
+
+
+
+	/** Setter
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+
+
+	/** Getter
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+
+
+
+
+	/** Setter
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+
+
+
+
+	/** Getter
+	 * @return the date_Creation
+	 */
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+
+
+
+
+	/** Setter
+	 * @param date_Creation the date_Creation to set
+	 */
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
 	}
 	
 }
